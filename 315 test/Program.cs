011b567d96659b6137e709045e294418fe0d186b
ASUS\USER <raer1332@gmail.com>
2018-03-15 17:49:43 +0800

    no message

---
+++ 315 test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _315_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.White;
+            // 設定變數
+            double rate = 0.3;
+
+            // desplay
+            Console.WriteLine("recent rate{0}", +rate);
+
+            //count
+            Console.WriteLine("1000convert into:"+(1000 * rate) + "NT dollars");
+
+            // enter# then count resualt
+            Console.Write("enter #:");
+            string input = Console.ReadLine();
+
+            // desplay resualt
+            double total = int.Parse(input) * rate;
+            Console.WriteLine ("get:" + total);
+
+
+            // end
+            Console.ReadLine();
+            
+        }
+    }
+}
